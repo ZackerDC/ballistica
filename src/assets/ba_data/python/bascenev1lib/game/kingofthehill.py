68f164fa761a3bf100dd9f391bf64f964d20854b
Zacker Tz <100242960+ZackerDC@users.noreply.github.com>
2024-12-01 19:23:00 -0500

    PW - kingofthehill.py
    Implementation of a new exclusive region for Happy Thought Map, to avoid obtaining points from the bottom of the platform

--- src/assets/ba_data/python/bascenev1lib/game/kingofthehill.py
+++ src/assets/ba_data/python/bascenev1lib/game/kingofthehill.py
@@ -181,18 +181,30 @@ class KingOfTheHillGame(bs.TeamGameActivity[Player, Team]):
                 'radius': 0.4,
                 'color': (0.2, 0.2, 0.2),
             },
-        )
         # Flag region.
         flagmats = [self._flag_region_material, shared.region_material]
-        bs.newnode(
-            'region',
-            attrs={
-                'position': self._flag_pos,
-                'scale': (1.8, 1.8, 1.8),
-                'type': 'sphere',
-                'materials': flagmats,
-            },
-        )
+        if self.map.getname() == "Happy Thoughts":
+        # Exclusive region for Happy Thoughts, to avoid 
+        # marking points from the bottom of the platform.
+            bs.newnode(
+                'region',
+                attrs={
+                    'position': (self._flag_pos[0], self._flag_pos[1]*1.06, self._flag_pos[2]),
+                    'scale': (3.4, 1.75, 0.8),
+                    'type': 'square',
+                    'materials': flagmats,
+                },
+            )
+        else:
+            bs.newnode(
+                'region',
+                attrs={
+                    'position': self._flag_pos,
+                    'scale': (1.8, 1.8, 1.8),
+                    'type': 'sphere',
+                    'materials': flagmats,
+                },
+            )
         self._update_scoreboard()
         self._update_flag_state()
 
